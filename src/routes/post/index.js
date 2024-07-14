e89dc10f88dd125c9a8a491ddf89a44ed64622b5
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-07-14 16:40:33 +0500

    Post validation

--- src/routes/post/index.js
+++ src/routes/post/index.js
@@ -2,20 +2,53 @@ import { Router } from "express";
 import postController from "../../controllers/post/index.js";
 import verifyToken from "../../middleware/auth/verifyToken.js";
 import checkTokenFromDb from "../../middleware/auth/checkTokenDb.js";
+import postValidator from "../../validators/post/index.js";
 
 const postRouter = Router();
 
-postRouter.post("/post/create", verifyToken, postController.createPost);
+postRouter.post(
+  "/post/create",
+  verifyToken,
+  checkTokenFromDb,
+  postValidator.post,
+  postController.createPost
+);
 
-postRouter.delete("/post/delete/:id", verifyToken, postController.deletePost);
+postRouter.delete(
+  "/post/delete/:id",
+  verifyToken,
+  checkTokenFromDb,
+  postController.deletePost
+);
 
-postRouter.post("/post/like/:id", verifyToken, postController.likeUnlikePost);
+postRouter.post(
+  "/post/like/:id",
+  verifyToken,
+  checkTokenFromDb,
+  postController.likeUnlikePost
+);
 
-postRouter.post("/post/comment/:id", verifyToken, postController.commentOnPost);
+postRouter.post(
+  "/post/comment/:id",
+  verifyToken,
+  checkTokenFromDb,
+  postValidator.comment,
+  postController.commentOnPost
+);
 
-postRouter.get("/post/all", verifyToken, postController.getAllPosts);
+postRouter.get(
+  "/post/all",
+  verifyToken,
+  checkTokenFromDb,
+  postController.getAllPosts
+);
 
-postRouter.get("/post/likes/:id", verifyToken, postController.getLikedPosts);
+postRouter.get(
+  "/post/likes/:id",
+  verifyToken,
+  checkTokenFromDb,
+  postController.getLikedPosts
+);
 
 postRouter.get(
   "/post/following",
